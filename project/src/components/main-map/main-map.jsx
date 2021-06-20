c3797aee571c4451c276768d1438681be304377e
Michael <kokin.m92@gmail.com>
2021-06-20 13:25:51 +0300

    Больше подробностей (часть 1)

--- project/src/components/main-map/main-map.jsx
+++ project/src/components/main-map/main-map.jsx
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
 import OffersProp from '../../pages/offer/offer.prop';
+import leaflet from "leaflet";
+import 'leaflet/dist/leaflet.css';
+
+import useMap from "../../hooks/useMap";
 
 function MainMap(props) {
   const { cardLocation, offersList } =  props;
   const locationList = offersList.map((offer) => offer.location);
+  const cityLocation = offersList[0].city.location;
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, cityLocation)
+  const defaultIcon = leaflet.icon({
+    iconUrl: 'img/pin.svg',
+    iconSize: [30, 30],
+    iconAnchor: [15, 30],
+  });
+  const activeIcon = leaflet.icon({
+    iconUrl: 'img/pin-active.svg',
+    iconSize: [30, 30],
+    iconAnchor: [15, 30],
+  });
 
-  console.log(cardLocation, locationList);
+  function checkSelectedMarker(location) {
+    return location.latitude === cardLocation.latitude && location.longitude === cardLocation.longitude
+  }
+
+  useEffect(() => {
+    if (map) {
+      map.setView([cityLocation.latitude, cityLocation.longitude], cityLocation.zoom);
+
+      locationList.map((location) => {
+        leaflet
+          .marker({
+            lat: location.latitude,
+            lng: location.longitude
+          }, {
+            icon: checkSelectedMarker(location) ? activeIcon : defaultIcon
+          })
+          .addTo(map);
+      })
+    }
+  }, [map, cityLocation, cardLocation]);
 
   return (
-    <div>
-      <div>
-        <b>Location list</b>
-        <div>{locationList.map((location, index) => {
-          const keyValue = index + location.zoom;
-          return (
-            <div key={keyValue}><b>hotel:</b> {location.latitude}, {location.longitude}, {location.zoom}</div>
-          );
-        })}
-        </div>
-      </div>
-      <br />
-      <div>
-        <b>Active hotel:</b>
-        <div>{cardLocation.latitude}, {cardLocation.longitude}, {cardLocation.zoom}</div>
-      </div>
-      <div className="cities__right-section">
-        <section className="cities__map map">
-        </section>
-      </div>
+    <div className="cities__right-section">
+      <section ref={mapRef} className="cities__map map" style={{ height: '100%' }}>
+      </section>
     </div>
   );
 }
